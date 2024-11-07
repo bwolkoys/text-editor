c0d41f7dde4bed4a52f84defa5283d2103b10f6b
Bradyn Wolkoys <bnwolkoys@gmail.com>
2024-11-07 15:40:56 -0800

    added logic to content that adds it to database and gets it from database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,27 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+
+  console.error('putDb not implemented');
+
+  const contactDb = await openDB('jate', 1); // Creates connection to database and version we want to use.
+    const tx = contactDb.transaction('jate', 'readwrite'); //creates a new transaction
+    const store = tx.objectStore('jate'); //opens the desired object store
+    const request = store.put({ id: 1, value: content }); //passes in the content
+    const result = await request; //gets confirmation from request 
+    console.log('🚀 - data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error('getDb not implemented');
+  const db = await openDB('jate', 1); // Creates connection to database and version we want to use.
+  const tx = db.transaction('jate', 'readonly'); // Creates new transaction 
+  const store = tx.objectStore('jate'); //opens the desired object store
+  const allContent = await store.getAll(); //.getAll() to get all content from database
+  console.log('retrieved data from database', allContent);
+  return allContent;
+};
 
 initdb();
