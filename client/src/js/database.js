0b4571b60d1d248a7e845f80bb53c7a15ed9e878
Bradyn Wolkoys <bnwolkoys@gmail.com>
2024-11-12 11:29:34 -0800

    updating from errors

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,8 +15,6 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
 
-  console.error('putDb not implemented');
-
   const intDb = await openDB('jate', 1); // Creates connection to database and version we want to use.
     const tx = intDb.transaction('jate', 'readwrite'); //creates a new transaction
     const store = tx.objectStore('jate'); //opens the desired object store
@@ -27,7 +25,7 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error('getDb not implemented');
+
   const intdb = await openDB('jate', 1); // Creates connection to database and version we want to use.
   const tx = intdb.transaction('jate', 'readonly'); // Creates new transaction 
   const store = tx.objectStore('jate'); //opens the desired object store
